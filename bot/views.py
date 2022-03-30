68dba620c31f6fc4115f1acb10093c9e3fb3c492
ibrahimsamson <toddler1998@yahoo.com>
2022-03-30 18:23:00 +0800

    added instructions on how to use  it

--- bot/views.py
+++ bot/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# import pywhatkit as pwk
+import pywhatkit as pwk
 from . import groups
 from django.views.generic import TemplateView
 image = ''    # example image =  'images/Hello.png'
@@ -17,13 +17,9 @@ def send_image(request, group_id, image, caption):
     return render(request, 'index.html', )
 
 # def send_txt(request, group_id, txt):
-def send_txt(request, group_id, txt):
-    # for group_id in groups:
-    #     pwk.sendwhatmsg_to_group_instantly(group_id, txt)
+def send_txt(request):
+    txt = request.GET.get('c_message')
+    for group_id in groups:
+        pwk.sendwhatmsg_to_group_instantly(group_id, txt)
 
-    if request.method == 'GET':
-        print(request.method)
     return render(request, 'index.html', )
-
-
-    # query = request.GET.get('query')
